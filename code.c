97475703c94ec7801ffdf4027746f2ba6ddd0cae
Nathan <ndbrewer@mtu.edu>
2023-04-23 16:00:51 -0400

    com

--- code.c
+++ code.c
@@ -6,85 +6,72 @@
 #include <netpbm/pbm.h>
 #include <netpbm/pgm.h>
 
-
-
-int parse_size(const char *arg, int *width, int *height) {
-    char *endptr;
-    const char *ptr = strchr(arg, 'x');
-    if (ptr == NULL || ptr == arg || *(ptr + 1) == '\0') {
-        return -1;
-    }
-    *width = strtol(arg, &endptr, 10);
-    if (*endptr != 'x') {
-        return -1;
-    }
-    *height = strtol(ptr + 1, &endptr, 10);
-    if (*endptr != '\0') {
-        return -1;
-    }
-    return 0;
-}
-
-int parse_pixel(const char *arg, int *row, int *col, unsigned char *value) {
-    char *endptr;
-    const char *ptr = strchr(arg, ',');
-    if (ptr == NULL || ptr == arg || *(ptr + 1) == '\0') {
-        return -1;
-    }
-    *row = strtol(arg, &endptr, 10);
-    if (*endptr != ',') {
-        return -1;
-    }
-    *col = strtol(ptr + 1, &endptr, 10);
-    if (*endptr != '=') {
-        return -1;
-    }
-    *value = strtol(endptr + 1, &endptr, 10);
-    if (*endptr != '\0' || *value > 255) {
-        return -1;
-    }
-    return 0;
-}
+#define MAX_PIXEL_VALUE 255
 
 int main(int argc, char *argv[]) {
-    int width, height;
-    unsigned char fill_value = 0;
-    int i, row, col;
-    unsigned char value;
-    FILE *output;
-    gray **image;
-
-    if (argc < 2 || parse_size(argv[1], &width, &height) != 0) {
-        fprintf(stderr, "Usage: %s WxH [R,C=X|X ...]\n", argv[0]);
-        return EXIT_FAILURE;
+    if (argc < 3) {
+        printf("Usage: %s WxH [R,C=X|X]...\n", argv[0]);
+        return 1;
     }
 
-    image = pbm_allocarray(width, height);
+    // Parse width and height from the first command line argument
+    char *size_str = strtok(argv[1], "x");
+    if (size_str == NULL) {
+        printf("Error: Invalid size argument\n");
+        return 1;
+    }
+    int width = atoi(size_str);
+    size_str = strtok(NULL, "x");
+    if (size_str == NULL) {
+        printf("Error: Invalid size argument\n");
+        return 1;
+    }
+    int height = atoi(size_str);
 
+    // Initialize image with all pixels set to 0
+    bit **image = pbm_allocarray(width, height);
     if (image == NULL) {
-        fprintf(stderr, "Failed to allocate memory\n");
-        return EXIT_FAILURE;
+        printf("Error: Failed to allocate image\n");
+        return 1;
     }
 
-    for (i = 2; i < argc; i++) {
-        if (isdigit(argv[i][0])) {
-            if (sscanf(argv[i], "%hhu", &fill_value) != 1) {
-                fprintf(stderr, "Invalid fill value: %s\n", argv[i]);
-                return EXIT_FAILURE;
+    // Parse and apply command line arguments
+    for (int i = 2; i < argc; i++) {
+        char *arg = argv[i];
+
+        if (strchr(arg, ',') != NULL) {
+            // Set pixel value
+            char *pos = strchr(arg, '=');
+            if (pos == NULL) {
+                printf("Error: Invalid pixel value argument\n");
+                return 1;
             }
-            pbm_clear(image, width, height, fill_value);
-        } else {
-            if (parse_pixel(argv[i], &row, &col, &value) != 0) {
-                fprintf(stderr, "Invalid pixel value: %s\n", argv[i]);
-                return EXIT_FAILURE;
-            }
-            if (row < 0 || row >= height || col < 0 || col >= width) {
-                fprintf(stderr, "Pixel out of range: %s\n", argv[i]);
-                return EXIT_FAILURE;
+            *pos = '\0';
+            int row = atoi(strtok(arg, ","));
+            int col = atoi(strtok(NULL, ","));
+            int value = atoi(pos + 1);
+            if (row < 0 || row >= height || col < 0 || col >= width || value < 0 || value > MAX_PIXEL_VALUE) {
+                printf("Error: Invalid pixel coordinates or value\n");
+                return 1;
             }
             image[row][col] = value;
+        } else {
+            // Fill entire image with value
+            int value = atoi(arg);
+            if (value < 0 || value > MAX_PIXEL_VALUE) {
+                printf("Error: Invalid fill value\n");
+                return 1;
+            }
+            for (int row = 0; row < height; row++) {
+                for (int col = 0; col < width; col++) {
+                    image[row][col] = value;
+                }
+            }
         }
     }
+
+    // Write image to file
+    FILE *output;
 
     output = fopen("image.pgm", "w");
     if (output == NULL) {
